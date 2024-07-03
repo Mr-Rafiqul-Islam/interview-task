cf89cf3549f531d8549c8722491753a9c57b7984
Mr. Rafiqul Islam <rafiqulislamemon05@gmail.com>
2024-07-03 20:59:24 +0600

    project assets setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'btn': '#BF885E',
+        'btn-hover': '#d9945f',
+        'btn2': '#856A54',
+        'btn-hover2': '#A38C7A',
+        'primary': "#473D35"
+      },
+      fontFamily: {
+        'roboto': ['Roboto', 'sans-serif'],
+        'anglecia': ['AngleciaProDisplay', 'sans-serif'],
+      },
+      backgroundImage:{
+        'hero': "url('/src/assets/images/1920x1064_bg.jpg')",
+      }
+    },
   },
   plugins: [],
 }
